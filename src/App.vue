4d016df7988979dcd2644d352bd6d3aa8819cad1
diegoG82 <diegogiardina@hotmail.it>
2023-04-26 09:17:08 +0200

    fixed appmain component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script>
 import { store } from './store.js'
 import AppHeader from './components/AppHeader.vue';
-import AppMain from './components/Appmain.vue';
+import AppMain from './components/AppMain.vue';
 
 export default {
 
